e6e277ceb4d1ac17890f1dc331c59e7fcd5e9831
ivan.k <vania.kravchenko1@gmail.com>
2019-09-27 18:58:36 +0300

    commit10

--- src/components/Profile/ProfileInfo/ProfileStatus.js
+++ src/components/Profile/ProfileInfo/ProfileStatus.js
@@ -4,18 +4,21 @@ import classes from "./ProfileInfo.module.css";
 export default function ProfileStatus(props) {
 
     const [editMode, setEditMode] = useState(false);
+    const [status, setStatus] = useState(props.status);
 
     return (
         <div>
             {!editMode &&
-                <div>
-                    <span onDoubleClick={() => setEditMode(true)}>{props.status}</span>
-                </div>
+            <div>
+                <span onDoubleClick={() => setEditMode(true)}>{status}</span>
+            </div>
             }
             {editMode &&
-                <div>
-                    <input type="text" value={props.status}/>
-                </div>
+            <div>
+                <input type="text" autoFocus={true} onBlur={() => setEditMode(false)}
+                       onChange={(e) => setStatus(e.target.value)} value={status}/>
+                <button onClick={() => setEditMode(false)}>Save</button>
+            </div>
             }
         </div>
     );
